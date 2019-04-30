ba084b7e3883c846f53bdb7b979024bceaf75d84
david.b.kessler <david.b.kessler@accenture.com>
2019-04-30 07:01:56 -0500

    part V

--- src/com/pairgood/adventure/Main.java
+++ src/com/pairgood/adventure/Main.java
@@ -19,23 +19,52 @@ public class Main {
 		
 		System.out.println("Welcome to Adventure" + name + "!");
 		
-		System.out.println("[1] Go to the store.");
-		System.out.println("[2] Go into the forest.");
-		System.out.println("[3] Talk to the townspeople.");
-		
-		int input = scanner.nextInt();
-		
-		if(input == 1) {
-			System.out.println("You bought a health potion and restored some health.");
-			money -= 1;
-			health += 1;
+		int input;
+		 
+		do {
+			System.out.println("[0] Quit.");
+			System.out.println("[1] Go to the store.");
+			System.out.println("[2] Go into the forest.");
+			System.out.println("[3] Talk to the townspeople.");
+			System.out.println("money: " + money + ", health: " + health);
 			
-		}else if(input == 2) {
-			System.out.println("You discovered treasure in the forest after a long day of hiking.");
-			money += 1;
-			health -= 1;
-		}else if(input == 3){
-			System.out.println("The townspeople say hi.");
-		}
+			input = scanner.nextInt();
+			
+			if(input == 0) {
+				continue;
+				
+			}else if(input == 1) {
+				if(money - 1 < 0) {
+					System.out.println("You don't have enough money.");
+					continue;
+				}
+				
+				if(health + 1 > 10) {
+					System.out.println("You have max health.");
+					continue;
+				}
+				
+				System.out.println("You bought a health potion and restored some health.");
+				money -= 1;
+				health += 1;
+				
+			}else if(input == 2) {
+	
+				if(health - 1 > 10) {
+					System.out.println("You don't have enough health.");
+					continue;
+				}
+				
+				System.out.println("You discovered treasure in the forest after a long day of hiking.");
+				money += 1;
+				health -= 1;
+			}else if(input == 3){
+				for(int i = 0; i< 5; i++) {
+					System.out.println("Someone say hi.");
+				}
+			}else {
+				System.out.println("Invalid item.");
+			}
+		} while(input != 0);
 	}
 }
