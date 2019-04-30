a413ccfa480512572a9624d0a312fa64f9e0bae7
david.b.kessler <david.b.kessler@accenture.com>
2019-04-29 20:00:26 -0500

    part IV

--- src/com/pairgood/adventure/Main.java
+++ src/com/pairgood/adventure/Main.java
@@ -18,5 +18,24 @@ public class Main {
 		
 		
 		System.out.println("Welcome to Adventure" + name + "!");
+		
+		System.out.println("[1] Go to the store.");
+		System.out.println("[2] Go into the forest.");
+		System.out.println("[3] Talk to the townspeople.");
+		
+		int input = scanner.nextInt();
+		
+		if(input == 1) {
+			System.out.println("You bought a health potion and restored some health.");
+			money -= 1;
+			health += 1;
+			
+		}else if(input == 2) {
+			System.out.println("You discovered treasure in the forest after a long day of hiking.");
+			money += 1;
+			health -= 1;
+		}else if(input == 3){
+			System.out.println("The townspeople say hi.");
+		}
 	}
 }
