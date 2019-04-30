4521e0f539bd3a0de3e7abe7ee7784110e936b5b
david.b.kessler <david.b.kessler@accenture.com>
2019-04-29 19:15:02 -0500

    part II

--- src/com/pairgood/adventure/Main.java
+++ src/com/pairgood/adventure/Main.java
@@ -3,6 +3,11 @@ package com.pairgood.adventure;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		int health = 10;
+		double money = 19.99;
+		String name = "Pogo";
+		
 		System.out.println("Welcome to Adventure!");
 	}
 }
