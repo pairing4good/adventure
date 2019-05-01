2b730310257deed31b7092aaab428205e0168d87
david.b.kessler <david.b.kessler@accenture.com>
2019-05-01 18:38:40 -0500

    part VI

--- src/com/pairgood/adventure/Main.java
+++ src/com/pairgood/adventure/Main.java
@@ -8,16 +8,16 @@ public class Main {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		int health = 10;
+		int health = getMaxHealth();
 		double money = 19.99;
 
-		System.out.println("What is your name?");
+		print("What is your name?");
 		
 		String name = scanner.nextLine();
 		
 		
 		
-		System.out.println("Welcome to Adventure" + name + "!");
+		print("Welcome to Adventure" + name + "!");
 		
 		int input;
 		 
@@ -35,36 +35,44 @@ public class Main {
 				
 			}else if(input == 1) {
 				if(money - 1 < 0) {
-					System.out.println("You don't have enough money.");
+					print("You don't have enough money.");
 					continue;
 				}
 				
-				if(health + 1 > 10) {
-					System.out.println("You have max health.");
+				if(health + 1 > getMaxHealth()) {
+					print("You have max health.");
 					continue;
 				}
 				
-				System.out.println("You bought a health potion and restored some health.");
+				print("You bought a health potion and restored some health.");
 				money -= 1;
 				health += 1;
 				
 			}else if(input == 2) {
 	
-				if(health - 1 > 10) {
-					System.out.println("You don't have enough health.");
+				if(health - 1 < 0) {
+					print("You don't have enough health.");
 					continue;
 				}
 				
-				System.out.println("You discovered treasure in the forest after a long day of hiking.");
+				print("You discovered treasure in the forest after a long day of hiking.");
 				money += 1;
 				health -= 1;
 			}else if(input == 3){
 				for(int i = 0; i< 5; i++) {
-					System.out.println("Someone say hi.");
+					print("Someone say hi.");
 				}
 			}else {
-				System.out.println("Invalid item.");
+				print("Invalid item.");
 			}
 		} while(input != 0);
+	}
+	
+	public static void print(String message) {
+		System.out.println("Adventure: " + message);
+	}
+	
+	public static int getMaxHealth() {
+		return 10;
 	}
 }
